fcf7abd6ab6d4ec3ea39c2dd1f983cfc7aab4a8b
Win <winjema@gmail.com>
2024-06-18 18:04:44 +0200

    updating js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var currentDate = new Date().toLocaleTimeString();
 
 console.log("top of main: ", currentDate);
+
+var minimumNumberOfHarvesters = 5;
+var numberOfHarvesters = _.sum(
+  Game.creeps,
+  (c) => c.memory.role == "harvester"
+);
+//arrow function to filter creeps to only count harvesters
+
+console.log(numberOfHarvesters);
