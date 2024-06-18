efa60c7178819d8d10bcf6cda5110d35815e64c4
Win <winjema@gmail.com>
2024-06-18 21:50:22 +0200

    updating main

--- main.js
+++ main.js
@@ -2,19 +2,39 @@ var currentDate = new Date().toLocaleTimeString();
 
 console.log("top of main: ", currentDate);
 
-function createHarvester() {
+function spawnHarvester() {
   console.log("creating Harvester");
+  var name = Game.spawns.Spawn1.spawnCreep(
+    [WORK, WORK, CARRY, MOVE],
+    undefined,
+    {
+      role: "harvester",
+      working: false,
+    }
+  );
 }
 
+function spawnUpgrader() {
+  console.log("creating Upgrader");
+  var name = Game.spawns.Spawn1.spawnCreep(
+    [WORK, WORK, CARRY, MOVE],
+    undefined,
+    {
+      role: "upgrader",
+      working: false,
+    }
+  );
+}
+var name = undefined;
+
 var minimumNumberOfHarvesters = 5;
-var numberOfHarvesters = _.sum(
-  Game.creeps,
-  (c) => c.memory.role == "harvester"
-);
+numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == "harvester");
 //arrow function to filter creeps to only count harvesters
 
 console.log(numberOfHarvesters);
 
 if (numberOfHarvesters < minimumNumberOfHarvesters) {
-  createHarvester();
+  spawnHarvester();
+} else {
+  spawnUpgrader();
 }
